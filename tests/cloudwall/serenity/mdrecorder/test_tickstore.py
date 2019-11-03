de313dde42f1bfc6fde6691823d09e36d4270bbf
Kyle Downey <kyle.downey@gmail.com>
2019-11-02 23:58:22 -0400

    Initial implementation of select operation

--- tests/cloudwall/serenity/mdrecorder/test_tickstore.py
+++ tests/cloudwall/serenity/mdrecorder/test_tickstore.py
@@ -1,5 +1,5 @@
 import datetime
-
+import numpy as np
 import pandas as pd
 
 from cloudwall.serenity.mdrecorder.tickstore import LocalTickstore, BiTimestamp
@@ -11,6 +11,10 @@ def test_tickstore(mocker: MockFixture):
     tickstore = LocalTickstore(Path('./COINBASE_PRO_ONE_MIN_BINS'))
 
     for i in range(31):
-        tickstore.insert('BTC-USD', BiTimestamp(datetime.date(2019, 10, i+1)), pd.DataFrame())
+        ticks = pd.DataFrame(np.random.randint(0, 100, size=(100, 4)), columns=list('ABCD'))
+        tickstore.insert('BTC-USD', BiTimestamp(datetime.date(2019, 10, i+1)), ticks)
+
+    df = tickstore.select('BTC-USD', start=datetime.datetime(2019, 10, 1), end=datetime.datetime(2019, 10, 15))
+    assert df.size == 6000
 
     tickstore.close()
