9d6f581baed23c6f02feaacac3d274e013c39417
Makas Tzavellas <3305216+mtzavellas@users.noreply.github.com>
2019-11-03 12:51:12 -0500

    Updated generator to take in parameters.

--- tests/cloudwall/serenity/mdrecorder/tick_generator.py
+++ tests/cloudwall/serenity/mdrecorder/tick_generator.py
@@ -4,29 +4,33 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 
 
-def generate_and_random_ticks():
-    ts_col_name = 'ts'
-    start = pd.to_datetime('2019-10-1')
-    end = pd.to_datetime('2019-10-31')
+def generate_and_random_ticks(
+        ts_col_name='ts',
+        start=pd.to_datetime('2019-10-1'),
+        end=pd.to_datetime('2019-10-31'),
+        col_list=list('ABCDEFGH'),
+        file_path='tick_testdata.parquet'):
     writer = None
-    filepath = 'tick_testdata.parquet'
     try:
         for i in range(31):
             ts_index = random_dates(start, end, 100)
             ts_index.name = ts_col_name
-            ticks = pd.DataFrame(np.random.randint(0, 100, size=(100, 4)), columns=list('ABCD'), index=ts_index)
-            writer = append_to_parquet(ticks, writer, filepath)
+            ticks = pd.DataFrame(np.random.randint(0, 100, size=(100, len(col_list))), columns=col_list, index=ts_index)
+            writer = append_to_parquet(ticks, writer, file_path)
     finally:
         writer.close()
 
-    in_pd = pq.read_table(filepath).to_pandas()
+    in_pd = pq.read_table(file_path).to_pandas()
     print(in_pd)
 
 
-def append_to_parquet(df: pd.DataFrame, writer: pq.ParquetWriter, filepath: str) -> pq.ParquetWriter:
+def append_to_parquet(
+        df: pd.DataFrame,
+        writer: pq.ParquetWriter,
+        file_path: str) -> pq.ParquetWriter:
     table = pa.Table.from_pandas(df)
     if writer is None:
-        writer = pq.ParquetWriter(filepath, table.schema)
+        writer = pq.ParquetWriter(file_path, table.schema)
     writer.write_table(table=table)
     return writer
 
